37cd04f344111b2448c348c3327990f30e058626
longobucco <longobuccofbc@gmail.com>
2024-08-01 23:13:27 -0300

    reconfiguring the test classes and database population for testing.

--- store-service/src/main/java/com/buccodev/bookstore/config/ConfigTest.java
+++ store-service/src/main/java/com/buccodev/bookstore/config/ConfigTest.java
@@ -5,9 +5,12 @@ import java.time.Instant;
 import java.time.LocalDate;
 
 import com.buccodev.bookstore.entity.*;
+import com.buccodev.bookstore.entity.dto.AddressDTO;
+import com.buccodev.bookstore.entity.dto.ClientDTO;
 import com.buccodev.bookstore.entity.enuns.PaymentMethod;
 import com.buccodev.bookstore.repositories.*;
-import com.buccodev.bookstore.services.OrderService;
+import com.buccodev.bookstore.services.*;
+import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +22,7 @@ import com.buccodev.bookstore.entity.enuns.Category;
 public class ConfigTest implements CommandLineRunner {
 
 	@Autowired
-	private PublisherRepository publisherRepo;
+	private PublisherRepository publisherRepository;
 
 	@Autowired
 	private BookRepository bookRepository;
@@ -43,13 +46,29 @@ public class ConfigTest implements CommandLineRunner {
 	private CardRepository cardRepository;
 
 	@Autowired
-	private OrderService service;
+	private Faker faker;
+
 
 	@Transactional
 	public void run(String... args) throws Exception {
 
 
+		for (int i = 0; i < 150; i ++){
 
+			Publisher publisher = new Publisher(null, faker.book().publisher(), faker.country().name());
+
+			publisherRepository.save(publisher);
+
+			int valueCat = faker.number().numberBetween(0, 6);
+
+			Book book = new Book(null, faker.book().title() + faker.number().numberBetween(1, 32), LocalDate.of(1933, 3, 8),
+					new BigDecimal(faker.number().randomDouble(4,1, 4)), Category.valueOf(valueCat) , publisher
+					);
+			book.setQuantityStock(faker.number().numberBetween(1,20));
+
+			bookRepository.save(book);
+
+		}
 
 
 	}
--- store-service/src/main/java/com/buccodev/bookstore/config/beans/ConfigBeans.java
+++ store-service/src/main/java/com/buccodev/bookstore/config/beans/ConfigBeans.java
@@ -1,4 +1,18 @@
 package com.buccodev.bookstore.config.beans;
 
+import com.github.javafaker.Faker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Locale;
+
+@Configuration
 public class ConfigBeans {
+
+    @Bean
+    public Faker faker(){
+
+        return new Faker(new Locale("pt-BR"));
+    }
+
 }
